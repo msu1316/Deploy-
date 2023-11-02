24f110fe38b70ebf6ec7bb24321cf38c79233a2f
msu1316 <30950191+msu1316@users.noreply.github.com>
2023-11-02 08:51:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,12 @@
 
 # In[1]:
 
-
+import streamlit as st
+import pandas as pd
+import base64
+import matplotlib.pyplot as plt
+import numpy as np
+import yfinance as yf
 import pandas as pd
 import numpy as np
 import tensorflow as tf
